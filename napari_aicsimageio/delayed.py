6a4703625c605321a7a79ae6d7bc6783cd4276c4
JacksonMaxfield <jmaxfieldbrown@gmail.com>
2020-03-20 11:37:52 -0700

    Fix references after renaming utils to core

--- napari_aicsimageio/delayed.py
+++ napari_aicsimageio/delayed.py
@@ -5,7 +5,7 @@ from typing import Optional
 
 from pluggy import HookimplMarker
 
-from . import utils
+from . import core
 
 ###############################################################################
 
@@ -15,5 +15,5 @@ napari_hook_implementation = HookimplMarker("napari")
 
 
 @napari_hook_implementation
-def napari_get_reader(path: utils.PathLike) -> Optional[utils.ReaderFunction]:
-    return utils.get_reader(path, compute=False)
+def napari_get_reader(path: core.PathLike) -> Optional[core.ReaderFunction]:
+    return core.get_reader(path, compute=False)
