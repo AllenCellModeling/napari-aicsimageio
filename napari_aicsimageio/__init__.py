16e0037b439e81aa7116e9e5db96fb28370acbba
JacksonMaxfield <jmaxfieldbrown@gmail.com>
2020-03-20 10:52:10 -0700

    Initial commit

---
+++ napari_aicsimageio/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for napari-aicsimageio."""
+
+__author__ = "Jackson Maxfield Brown"
+__email__ = "jacksonb@alleninstitute.org"
+# Do not edit this string manually, always use bumpversion
+# Details in CONTRIBUTING.md
+__version__ = "0.1.0"
+
+
+def get_module_version():
+    return __version__
+
+
+from .example import Example  # noqa: F401
---
+++ setup.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""The setup script."""
+
+from setuptools import find_packages, setup
+
+with open("README.md") as readme_file:
+    readme = readme_file.read()
+
+test_requirements = [
+    "codecov",
+    "flake8",
+    "black",
+    "pytest",
+    "pytest-cov",
+    "pytest-raises",
+]
+
+setup_requirements = [
+    "pytest-runner",
+]
+
+dev_requirements = [
+    "bumpversion>=0.5.3",
+    "coverage>=5.0a4",
+    "flake8>=3.7.7",
+    "ipython>=7.5.0",
+    "m2r>=0.2.1",
+    "pytest>=4.3.0",
+    "pytest-cov==2.6.1",
+    "pytest-raises>=0.10",
+    "pytest-runner>=4.4",
+    "Sphinx>=2.0.0b1",
+    "sphinx_rtd_theme>=0.1.2",
+    "tox>=3.5.2",
+    "twine>=1.13.0",
+    "wheel>=0.33.1",
+]
+
+interactive_requirements = [
+    "altair",
+    "jupyterlab",
+    "matplotlib",
+]
+
+requirements = []
+
+extra_requirements = {
+    "test": test_requirements,
+    "setup": setup_requirements,
+    "dev": dev_requirements,
+    "interactive": interactive_requirements,
+    "all": [
+        *requirements,
+        *test_requirements,
+        *setup_requirements,
+        *dev_requirements,
+        *interactive_requirements
+    ]
+}
+
+setup(
+    author="Jackson Maxfield Brown",
+    author_email="jacksonb@alleninstitute.org",
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+    description="AICSImageIO bindings for napari",
+    entry_points={
+        "console_scripts": [
+            "my_example=napari_aicsimageio.bin.my_example:main"
+        ],
+    },
+    install_requires=requirements,
+    license="MIT license",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    include_package_data=True,
+    keywords="napari, aicsimageio",
+    name="napari-aicsimageio",
+    packages=find_packages(exclude=["tests", "*.tests", "*.tests.*"]),
+    python_requires=">=3.6",
+    setup_requires=setup_requirements,
+    test_suite="napari_aicsimageio/tests",
+    tests_require=test_requirements,
+    extras_require=extra_requirements,
+    url="https://github.com/AllenCellModeling/napari-aicsimageio",
+    # Do not edit this string manually, always use bumpversion
+    # Details in CONTRIBUTING.rst
+    version="0.1.0",
+    zip_safe=False,
+)
