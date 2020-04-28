f01b3f35f7d5734af338eb32a2c3ee2651dcd085
JacksonMaxfield <jmaxfieldbrown@gmail.com>
2020-04-28 09:37:55 -0700

    Add comment about napari version in setup.py

--- setup.py
+++ setup.py
@@ -39,7 +39,9 @@ dev_requirements = [
 
 requirements = [
     "aicsimageio>=3.1.4",
-    #"napari>=0.3.0",
+    # napari plugins is the `napari>=0.3.0` release
+    # If someone is trying to use plugins they should have everything they need
+    # for this package to work
     "napari_plugin_engine>=0.1.4",
 ]
 
