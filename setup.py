1f41c5be23d2a284dcd52e1274b537c419e50ee2
JacksonMaxfield <jmaxfieldbrown@gmail.com>
2021-06-29 11:00:39 -0700

    Bump version: 0.2.0 → 0.3.0

--- setup.py
+++ setup.py
@@ -93,6 +93,6 @@ setup(
     url="https://github.com/AllenCellModeling/napari-aicsimageio",
     # Do not edit this string manually, always use bumpversion
     # Details in CONTRIBUTING.rst
-    version="0.2.0",
+    version="0.3.0",
     zip_safe=False,
 )
