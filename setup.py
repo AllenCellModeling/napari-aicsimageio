3dde95793864c7a77b8566d41cd87f563faa67d3
JacksonMaxfield <jmaxfieldbrown@gmail.com>
2021-07-11 23:16:24 -0700

    Bump version: 0.3.3 → 0.3.4

--- setup.py
+++ setup.py
@@ -101,6 +101,6 @@ setup(
     },
     # Do not edit this string manually, always use bumpversion
     # Details in CONTRIBUTING.rst
-    version="0.3.3",
+    version="0.3.4",
     zip_safe=False,
 )
