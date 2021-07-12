9dfae24b630251bf8adcfc2a1280a150f8fb9c65
Jackson Maxfield Brown <jmaxfieldbrown@gmail.com>
2021-07-11 22:57:43 -0700

    Add fsspec[http] to install deps for remote read

--- setup.py
+++ setup.py
@@ -41,6 +41,7 @@ dev_requirements = [
 
 requirements = [
     "aicsimageio[all]~=4.0.2",
+    "fsspec[http]",  # no version pin, we pull from aicsimageio
     "napari~=0.4.10",
     "napari_plugin_engine~=0.1.4",
 ]
